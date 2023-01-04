0a5814d4cf3104b21d7812b0d1c736a452338347
Jay Kumar Bharti <60088194+jayyadav362@users.noreply.github.com>
2023-01-04 23:31:00 +0530

    latest

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,16 @@
 // import using require
 
 // declare class
+import Shape from './shape';
 
+// declare class
+module.exports = class Circle extends Shape{
+     constructor(){
+      super();
+     }
+  calculateArea(){
+    console.log("calculated area from Circle class");
+  }
+}
 
 // export class using module.exports
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,13 @@
 // declare class
+module.exports = class Shape{
+    constructor(){
+        this.color = "red";
+    }
+    drawShape() {
+    }
+    calculateArea(){
+    console.log("calculated area using Shape");
+    } 
+}
 
 // export class using module.exports
